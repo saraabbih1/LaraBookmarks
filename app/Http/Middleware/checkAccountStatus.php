a016afce208b03d3c9777512e3f9ee898fc43c31
saraabbih1 <abbihsara30@gmail.com>
2026-02-02 16:58:15 +0100

    feat:la prtie de checkaccountstatus

--- app/Http/Middleware/checkAccountStatus.php
+++ app/Http/Middleware/checkAccountStatus.php
@@ -4,17 +4,22 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class checkAccountStatus
+class CheckAccountStatus
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        if (Auth::check() && Auth::user()->is_active == false) {
+            Auth::logout();
+
+            return redirect()->route('login')
+                ->withErrors([
+                    'email' => 'Votre compte est désactivé. Veuillez contacter l’administrateur.'
+                ]);
+        }
+
         return $next($request);
     }
 }
