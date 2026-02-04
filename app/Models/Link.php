d17aa37709886ff5751a45790d11fc78b06c4734
saraabbih1 <abbihsara30@gmail.com>
2026-02-04 10:13:36 +0100

    feat:la relation tags et link belongstomany

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -10,4 +10,7 @@ class Link extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
