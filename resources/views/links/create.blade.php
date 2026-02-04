97d73f043b853a6bbc84fbecb6487c106b5a8042
saraabbih1 <abbihsara30@gmail.com>
2026-02-04 10:29:41 +0100

    feat:ajoute la prtie tag à creation de link blade

--- resources/views/links/create.blade.php
+++ resources/views/links/create.blade.php
@@ -9,6 +9,11 @@
             <option value="{{ $cat->id }}">{{ $cat->name }}</option>
         @endforeach
     </select>
+    <select name="tags[]" multiple>
+        @foreach($tags as $tag)
+        <option value="{{$tag->id}}">{{$tag->name}}</option>
+        @endforeach
+    </select>
 
     <button>Ajouter</button>
 </form>
