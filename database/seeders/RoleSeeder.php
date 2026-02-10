725af8fb4565fd5caf602cfa5f0f42c31cd563eb
saraabbih1 <abbihsara30@gmail.com>
2026-02-10 12:26:40 +0100

    feat:seedroles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Role;
 class RoleSeeder extends Seeder
 {
     /**
@@ -12,6 +12,8 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+         Role::create(['name' => 'admin']);
+        Role::create(['name' => 'editor']);
+        Role::create(['name' => 'viewer']);
     }
 }
