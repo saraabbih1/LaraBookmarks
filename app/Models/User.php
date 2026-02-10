ccbb0b5cf5a09334b574aaabc4eaeca3a8c03afc
saraabbih1 <abbihsara30@gmail.com>
2026-02-10 12:22:40 +0100

    feat:remplissage de rolrmodel et user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
+    public function hasRole($role){
+        return $this->roles()->where('name',$role)->exists();
+    }
 }
